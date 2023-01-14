61f25f374b6c44b41bee997575736eb0f1c93b53
Daniil <daniil_sharukha@icloud.com>
2023-01-14 14:27:05 +0600

    + offcanvas, svg fill, links

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -3,6 +3,24 @@ var __webpack_exports__ = {};
 /*!**************************!*\
   !*** ./src/js/script.js ***!
   \**************************/
+function toggleOffcanvasMenu() {
+    const menuOpen = document.querySelector('.burger'),
+          menuClose = document.querySelector('.offcanvas__menu-close'),
+          menuStatus = document.querySelector('.offcanvas');
 
+    menuOpen.addEventListener('click', () => {
+        menuStatus.classList.add('offcanvas-active');
+        document.body.style.overflow = 'hidden';
+
+
+    });
+    menuClose.addEventListener('click', () => {
+        menuStatus.classList.remove('offcanvas-active');
+        document.body.style.overflow = '';
+    });
+}
+
+toggleOffcanvasMenu();
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+function toggleOffcanvasMenu() {
+    const menuOpen = document.querySelector('.burger'),
+          menuClose = document.querySelector('.offcanvas__menu-close'),
+          menuStatus = document.querySelector('.offcanvas');
+
+    menuOpen.addEventListener('click', () => {
+        menuStatus.classList.add('offcanvas-active');
+        document.body.style.overflow = 'hidden';
+
+
+    });
+    menuClose.addEventListener('click', () => {
+        menuStatus.classList.remove('offcanvas-active');
+        document.body.style.overflow = '';
+    });
+}
+
+toggleOffcanvasMenu();
